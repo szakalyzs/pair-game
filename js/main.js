5cc25e673daa3b0b7de0fbcafdaf3afd2d9ef50c
unknown <szakalyzs@gmail.com>
2020-12-05 16:03:49 +0100

    Cards exist.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 'use strict';
 
+(function cardClick() {
+    let cardBoxes = document.querySelectorAll('.card');
+    for (let i = 0; i < cardBoxes.length; i++) {
+        cardBoxes[i].addEventListener('click', () => {
+            cardBoxes[i].setAttribute('class','card--face');
+            cardBoxes[i].textContent = i;
+        });
+        
+    }
+})();
